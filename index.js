fce71e788789b8d87fb16ad9ea0a1680d226dc36
Riccardo Bertossa <rbertoss@materys.com>
2024-04-05 10:28:13 +0000

    added correct handling of U numpy unicode type

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import fetch from 'cross-fetch';
 
+class StringFromCodePoint extends String {
+    constructor(...args) {
+        const uint32 = new Uint32Array(...args);
+        const number_arr = Array.from(uint32);
+        const str = String.fromCodePoint(...number_arr);
+        console.log("str",str);
+        super(str);
+    }
+}
+
 class npyjs {
 
     constructor(opts) {
@@ -93,13 +103,22 @@ class npyjs {
             dtype = {
                 name: "unicode",
                 size: parseInt(header.descr.substring(2)),
-                arrayConstructor: Uint16Array
+                arrayConstructor: StringFromCodePoint
             };
         }
-        const nums = new dtype["arrayConstructor"](
+        var nums = new dtype["arrayConstructor"](
             arrayBufferContents,
             offsetBytes
         );
+        //convert to a plain string array the StringFromCodePoint object
+        if (dtype.name === "unicode") {
+            const nums_ = String(nums);
+            nums = new Array();
+            //split the string into an array of strings with length dtype.size
+            for (let i = 0; i < nums_.length; i += dtype.size) {
+                nums.push(nums_.substring(i, i + dtype.size));
+            }
+        }
         return {
             dtype: dtype.name,
             data: nums,
